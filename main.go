765be2950086ba6884eab27a1ab3a7cfbc4f35d5
Ibrahem Khalil <33176106+vampirekiddo@users.noreply.github.com>
2022-08-09 22:23:08 +0200

    interface example

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+type shape interface {
+	getArea() float64
+}
+
+type triangle struct {
+	height float64
+	base   float64
+}
+
+type square struct {
+	sideLength float64
+}
+
+func main() {
+	sq := square{sideLength: 10}
+	tr := triangle{height: 10, base: 10}
+	printArea(sq)
+	printArea(tr)
+}
+
+func (s square) getArea() float64 {
+	return s.sideLength * s.sideLength
+}
+
+func (t triangle) getArea() float64 {
+	return t.base * t.height * 0.5
+}
+
+func printArea(s shape) {
+	fmt.Print(s.getArea(), "\n")
+}
